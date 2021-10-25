feab61715fa62612eb366701242c617d961534ab
xubuntus <nikita.dmd@gmail.com>
2021-10-26 00:50:56 +0300

    account gui

---
+++ SB-2.3.1/AccountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AccountViewController.swift
+//  SB-2.3.1
+//
+//  Created by xubuntus on 25.10.2021.
+//
+
+import UIKit
+
+class AccountViewController : UIViewController {
+    
+    override func viewDidLoad() {
+//        let layer = CAGradientLayer()
+//        layer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height / 2)
+//        layer.colors = [UIColor.systemPink.cgColor, UIColor.blue.cgColor]
+//        view.layer.addSublayer(view.layer.sublayers[0]!)
+    }
+}
