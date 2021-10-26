3f5b636eca204e0a7322d77dc0a0fbc4671c9dee
xubuntus <nikita.dmd@gmail.com>
2021-10-26 19:58:50 +0300

    ready for basic requirements

--- SB-2.3.1/AccountViewController.swift
+++ SB-2.3.1/AccountViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class AccountViewController : UIViewController {
     
+    @IBOutlet var welcomeOutlet: UILabel!
+    
+    var username = ""
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeOutlet.text = "Welcome, \(username)!"
 //        let layer = CAGradientLayer()
 //        layer.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height / 2)
 //        layer.colors = [UIColor.systemPink.cgColor, UIColor.blue.cgColor]
 //        view.layer.addSublayer(view.layer.sublayers[0]!)
+    }
+    
+    @IBAction func logoutButtonPressed() {
+        dismiss(animated: true)
     }
 }
--- SB-2.3.1/ViewController.swift
+++ SB-2.3.1/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var usernameOutlet: UITextField!
+    @IBOutlet var passwordOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard (usernameOutlet.text == "Nik" || usernameOutlet.text == "Nikita"), passwordOutlet.text == "password" else {
+            showAlert(title: "Invalid username or password", message: "Please enter correct username or password")
+            passwordOutlet.text = ""
+            return
+        }
+        guard let accountVC = segue.destination as? AccountViewController else { return }
+        accountVC.username = usernameOutlet.text!
+    }
+    
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        guard let _ = unwindSegue.source as? AccountViewController else { return }
+        usernameOutlet.text = ""
+        passwordOutlet.text = ""
+    }
+    
+    @IBAction func forgotUsernamePressed() {
+        showAlert(title: "Oops!", message: "Your username is Nik or Nikita 🤫")
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        showAlert(title: "Oops!", message: "Your password is password 🤔")
+    }
 }
 
+extension ViewController {
+    private func showAlert(title: String, message: String) -> Void {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+}
